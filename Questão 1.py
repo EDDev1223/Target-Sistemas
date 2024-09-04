8b924ac6973c00b022daf305c1910234d6adffdc
Edemilson Machado Viana Junior <edemilsonjr_sbc@hotmail.com>
2024-09-03 22:47:39 -0300

    Criação de Questão 1

---
+++ Questão 1.py
@@ -0,0 +1,15 @@
+# Variaveis
+
+indice = 13;
+soma = 0;
+K = 0;
+
+# Calculo da soma
+
+while K < indice:
+    K = K + 1
+    soma = soma + K
+
+    print (f"A soma é: {soma}")
+
+    # A soma é igual a 91!
